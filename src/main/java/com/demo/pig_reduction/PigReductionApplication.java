06f6185b70c4fcc2463136cc8058ef217a119545
xiaobing <2420709800@qq.com>
2019-12-09 19:05:51 +0800

    frist

---
+++ src/main/java/com/demo/pig_reduction/PigReductionApplication.java
@@ -0,0 +1,13 @@
+package com.demo.pig_reduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigReductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigReductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/pig_reduction/PigReductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.pig_reduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigReductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
